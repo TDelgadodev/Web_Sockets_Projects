f28521fd6d9edfefa5def1008e045e9ef6f8d6c8
Thiago <thiagodelgado.dev@gmail.com>
2023-11-28 10:17:09 -0300

    Add database using Turso DB

--- chat-with-node-sql-socket.io/server/index.js
+++ chat-with-node-sql-socket.io/server/index.js
@@ -1,15 +1,59 @@
 import express from "express";
-import logger from 'morgan';
+import logger from "morgan";
+import dotenv from "dotenv";
+import { createClient } from "@libsql/client";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
+dotenv.config();
 const port = process.env.PORT || 3000;
 
 const app = express();
-app.use(logger('dev'))
-
-app.get("/", (req, res) => {
-  res.sendFile(process.cwd() + '/client/index.html');
+const server = createServer(app);
+const io = new Server(server, {
+  connectionStateRecovery: {},
 });
 
-app.listen(port, () => {
-    console.log(`🚀 Server listening on port ${port}`);
-})
+const db = createClient({
+  url: "libsql://coherent-black-canary-tdelgadodev.turso.io",
+  authToken: process.env.DB_TOKEN,
+});
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT
+  );
+`);
+
+io.on("connection", (socket) => {
+  console.log("A user has connected!");
+
+  socket.on("disconnect", () => {
+    console.log("A user has disconnected");
+  });
+
+  socket.on("chat message", async (msg) => {
+    let result;
+    try {
+      result = await db.execute({
+        sql: "INSERT INTO messages (content) VALUES (:msg)",
+        args: { msg },
+      });
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+    io.emit("chat message", msg, result.lastInsertRowid.toString());
+  });
+});
+
+app.use(logger("dev"));
+
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/client/index.html");
+});
+
+server.listen(port, () => {
+  console.log(`🚀 Server listening on port ${port}`);
+});
