d5e716bc5ff9b83a1b13ea9636b1043e02f4c634
Thiago <thiagodelgado.dev@gmail.com>
2023-11-28 11:45:02 -0300

    Add scroll and fix little errors on table

--- chat-with-node-sql-socket.io/server/index.js
+++ chat-with-node-sql-socket.io/server/index.js
@@ -22,30 +22,53 @@ const db = createClient({
 await db.execute(`
   CREATE TABLE IF NOT EXISTS messages (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    content TEXT
+    content TEXT,
+    user TEXT
   );
 `);
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
   console.log("A user has connected!");
 
   socket.on("disconnect", () => {
-    console.log("A user has disconnected");
+    console.log("An user has disconnected");
   });
 
   socket.on("chat message", async (msg) => {
     let result;
+    const username = socket.handshake.auth.username ?? "anonymous";
     try {
       result = await db.execute({
-        sql: "INSERT INTO messages (content) VALUES (:msg)",
-        args: { msg },
+        sql: "INSERT INTO messages (content, user) VALUES (:msg, :username)",
+        args: { msg, username },
       });
     } catch (error) {
       console.log(error);
       return;
     }
-    io.emit("chat message", msg, result.lastInsertRowid.toString());
+    io.emit("chat message", msg, result.lastInsertRowid.toString(), username);
   });
+
+  if (!socket.recovered) {
+    try {
+      const results = await db.execute({
+        sql: "SELECT id, content, user FROM messages WHERE id > ?",
+        args: [socket.handshake.auth.serverOffset ?? 0],
+      });
+
+      results.rows.forEach((row) => {
+        socket.emit(
+          "chat message",
+          row.content,
+          row.id.toString(),
+          row.user
+        );
+      });
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+  }
 });
 
 app.use(logger("dev"));
